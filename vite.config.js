f6fc36dbd178a2d6d40cc709af985b5d6ad5f087
BIEH08 <isaacescamilla.h1998@gmail.com>
2024-08-09 08:09:00 -0600

    Second commit and build, is ready to deploy on GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "bieh08",
-    project: "javascript-react-iphone-15-pro-clone"
-  })],
+	plugins: [
+		react(),
+		sentryVitePlugin({
+			org: "bieh08",
+			project: "javascript-react-iphone-15-pro-clone",
+		}),
+	],
+	base: "/iPhone15ProClone/",
 
-  build: {
-    sourcemap: true
-  }
-})
+	build: {
+		sourcemap: true,
+	},
+});
